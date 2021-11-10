5bef6262e4b83a3a1f16d6efb33777a0d3418820
jericepon <jericepon@gmail.com>
2021-11-10 13:04:20 +0800

    Todo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from './app/store'
+import { add, complete } from './features/todo/todoSlice'
 
-function App() {
-    const [count, setCount] = useState(0)
+const App = () => {
+    const dispatch = useDispatch()
+    const todos = useSelector((state: RootState) => state.todos)
+    const [todo, setTodo] = useState('')
 
     return (
         <div>
-            Vite + React
+            <input type="text" name="todo" value={todo} onChange={e => setTodo(e.target.value)} placeholder="type something" />
+            <br />
+            <button onClick={() => {
+                if (todo !== '')
+                {
+                    dispatch(add(todo))
+                    setTodo('')
+                }
+            }} >Add</button>
+            <ul>
+                {todos && todos.map(({ text, done, id }, idx) => (
+                    <li key={idx}>{text} <button onClick={() => dispatch(complete(id))}>Complete</button></li>
+                ))}
+            </ul>
         </div>
     )
 }
--- src/features/todo/todoSlice.ts
+++ src/features/todo/todoSlice.ts
@@ -1,35 +1,37 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../../app/store";
 
-interface TodoInterface {
-    id: number;
-    done: boolean;
-    text: string;
+interface TodoItemInterface {
+    id: number
+    text: string
+    done: boolean
 }
 
-interface TodoSliceState {
-    todos: TodoInterface[]
-}
+type TodoList = TodoItemInterface[]
 
-const initialState: TodoSliceState = {
-    todos: []
-}
+const initialState: TodoList = []
 
 export const todoSlice = createSlice({
     name: 'todos',
     initialState,
     reducers: {
-        add: ({ todos }, { payload }: PayloadAction<string>) => {
-            todos = [
-                ...todos,
+        add: (state, { payload }: PayloadAction<string>) => state = [...state, { id: state.length, text: payload, done: false, }],
+        complete: (state, { payload }: PayloadAction<number>) => {
+            state.map(todo => {
+                if (todo.id === payload)
                 {
-                    id: todos.length,
-                    text: payload,
-                    done: false,
+                    return {
+                        ...todo,
+                        done: todo.done = !todo.done
+                    }
                 }
-            ]
-        },
-        remove: ({ todos }, action: PayloadAction<number>) => {
-            todos = todos.filter(({ id }) => id !== action.payload.id)
+
+                return todo
+            })
+            // state = state.filter(({ id }) => id !== action.payload)
         }
     }
 })
+
+export const { add, complete } = todoSlice.actions
+export default todoSlice.reducer
