93e47dc4c3162a94522550e505b0eb4c70dbc583
jericepon <jericepon@gmail.com>
2021-11-14 21:14:25 +0800

    Setup material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import { Container, CssBaseline } from '@mui/material'
 
 const App = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <CssBaseline />
+            <Container>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod architecto dicta enim dolor magnam deserunt expedita id cum maiores debitis, maxime in doloremque asperiores blanditiis aliquam odit totam. Consectetur, aspernatur?
+            </Container>
+        </>
     )
 }
 
