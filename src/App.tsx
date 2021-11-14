918729cfd198324a26d5c5572bb1b28b277c7c0c
jericepon <jericepon@gmail.com>
2021-11-14 18:14:43 +0800

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
+import React from 'react'
 
-function App() {
-    const [count, setCount] = useState(0)
-
+const App = () => {
     return (
         <div>
-            Vite + React
+            
         </div>
     )
 }
